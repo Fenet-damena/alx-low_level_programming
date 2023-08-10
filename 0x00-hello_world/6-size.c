e701a9e4816aa7e8d437c858de00ad67ab5fb47d
Fenet-damena <fenetdamena74@gmail.com>
2023-08-10 07:34:06 -0700

    linux

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,5 @@ int main(void)
 	printf("size of an int: %lu byte(s) \n", sizeof(b));
 	printf("size of a long int: %lu byte(s) \n", sizeof(c));
 	printf("size of a long long int: %lu byte(s) \n", sizeof(d));
-	printf("size of a flost: %lu byte(s) \n", sizeof(e));
+	printf("size of a float: %lu byte(s) \n", sizeof(e));
 }
