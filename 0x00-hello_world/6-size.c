975e4cdccda4f41fcea5931623657c5ebe7a71ed
Fenet-damena <fenetdamena74@gmail.com>
2023-08-10 14:13:50 -0700

    alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,19 @@
 /**
  * main - this is main function
  *
- * Return:(0)SUCCESS
+ * Return: 0 at the end
  */
 int main(void)
-{       char a;
-        int b;
-        long int c;
-        long long int d;
-        float e;
-	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-        printf("Size of an int: %lu byte(s)\n", sizeof(b));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-        printf("Size of a float: %lu byte(s)\n", sizeof(e));	
-	return (0);
+{	char a;
+	 int b;
+	 long int c;
+	 long long int d;
+	 float e;
+	 printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	 printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	 printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	 return (0);
 }
+
