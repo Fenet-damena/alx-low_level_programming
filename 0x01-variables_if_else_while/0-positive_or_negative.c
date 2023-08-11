87e513471c6c202185b75d1fa8c8104badba571c
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 01:35:46 -0700

    chech if the number is pos,neg,zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is main function
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	if (n == 0)
+	{
+		printf("is positive\n");
+	}
+	if (n < 0)
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}
