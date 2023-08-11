2619cd297fbe1696d879af4338ec7cfbee4acc5d
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 07:37:45 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
