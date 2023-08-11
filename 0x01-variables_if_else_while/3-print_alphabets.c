919042a9f5cb593859b744f8c18aa51b9f6b5829
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 06:18:45 -0700

    low level

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		( d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
