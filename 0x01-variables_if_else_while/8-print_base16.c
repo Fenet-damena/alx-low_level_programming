b707c3ed385f1cbf018a3d3a4505564b6d819e65
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 08:42:45 -0700

    alx

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,12 @@ int main(void)
 {
 	int i;
 
+	char  j;
+
 	for (i = 0 ; i < 10 ; i++)
-		printf("%d", i);
-	printf("\n");
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
 	return (0);
 }
