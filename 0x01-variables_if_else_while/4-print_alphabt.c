76d16dcc75d75e0ab13710be52b127424323b8d3
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 07:07:45 -0700

    low level

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	return (0);
+}
