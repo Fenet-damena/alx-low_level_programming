321dcd6f137d3165d18cfef1a328a10cacd4cca1
Fenet-damena <fenetdamena74@gmail.com>
2023-08-11 04:21:14 -0700

    low level programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,9 @@ int main(void)
 {	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastdigit = n%10;
+	int lastdigit;
+
+	lastdigit = n % 10;
 
 	if (lastdigit > 5)
 		printf("and is greater than 5\n");
