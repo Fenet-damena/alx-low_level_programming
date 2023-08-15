3a2f6ccd6c128aeab1f46d887892ab54badc938d
Fenet-damena <fenetdamena74@gmail.com>
2023-08-15 12:43:01 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	string a = "_putchar";
+
+	printf("%s/n", a);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	int i;
+	
+	char j;
+
+	j='0';
+	for (i = 0 ; i <= 9 ; i++)
+		putchar( j + (i + '0'));
+	
+
