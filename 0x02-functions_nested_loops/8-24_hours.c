fb0ccdd941d2039dca5cd13c3ce779a7879204b0
Fenet-damena <fenetdamena74@gmail.com>
2023-08-16 12:00:06 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,24 @@
 void jack_bauer(void)
 {
 	int i;
-	int j;while (i < 24) 
-	{j = 0;
+
+	int j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar(j / 10 + '0');
+			_putchar(j / 10 + '0');
 			_putchar('\n');
 			j++;
 		}
 		i++;
 	}
 }
+
