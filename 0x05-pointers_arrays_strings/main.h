43912fb61bcbbb614005ba8051b5e03869768d16
Fenet-damena <fenetdamena74@gmail.com>
2023-08-23 10:17:13 -0700

    alx

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN_h
-#define MAIN_h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <unist.h>
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
