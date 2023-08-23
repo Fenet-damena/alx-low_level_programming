8a4f07b46671dacc2f3865b8312eb94a23e2db96
Fenet-damena <fenetdamena74@gmail.com>
2023-08-23 11:25:40 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - the function that return the length of string
+ * @str: string to print
+ * Return the length of stsring
+ */
+int _strlen(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
