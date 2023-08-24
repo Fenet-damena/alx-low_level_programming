2de342bd444408e0a03220678e63d0d0fd129661
Fenet-damena <fenetdamena74@gmail.com>
2023-08-24 03:15:26 -0700

    print arry

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - is to print arry
+ * @a: is arru=y to be  printed
+ * @n: is number of element
+ * Return: elment in arry
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+			putchar(' ');
+			i++;
+		}
+	}
+	putchar('\n');
+}
+
+
