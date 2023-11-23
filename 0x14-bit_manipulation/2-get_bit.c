aea57c5f59f7ed4b7743c8677d51cdac65a4631b
Fenet-damena <fenetdamena74@gmail.com>
2023-11-23 14:32:13 -0800

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,17 +14,12 @@ int get_bit(unsigned long int n, unsigned int index)
     if (n == 0 && index < 64)
         return 0;
 
-    while (i <= index)
+    while (i < index)
     {
-        if (i == index)
-        {
-            return (n & 1);
-        }
-        
         n >>= 1;
         i++;
     }
 
-    return -1;
+    return (n & 1);
 }
 
