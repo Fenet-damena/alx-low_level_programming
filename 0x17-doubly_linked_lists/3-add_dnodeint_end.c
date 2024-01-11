f423b3590a48584004ed715b87c3c3057b1c658d
Fenet-damena <fenetdamena74@gmail.com>
2024-01-11 05:22:30 -0800

    alx

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *temp = *head;
+	dlistint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -22,14 +22,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return (new_node);
 	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	temp->next = new_node;
-	new_node->prev = temp;
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
 
 	return (new_node);
 }
